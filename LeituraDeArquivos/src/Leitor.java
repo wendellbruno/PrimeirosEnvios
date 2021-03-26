52e34e74a6135378699c897d2755aa8db2b47c93
wendellbruno <wendell_101@hotmail.com>
2021-03-26 16:38:56 -0300

    Versão 2.0

--- LeituraDeArquivos/src/Leitor.java
+++ LeituraDeArquivos/src/Leitor.java
@@ -8,34 +8,15 @@ public class Leitor {
     public static void main(String[] args) {
 
         String path = "c:\\temp\\in.txt";
-        FileReader fr = null;
-        BufferedReader br = null;
 
-        try{
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
-
+        try(BufferedReader br = new BufferedReader(new FileReader(path))){
             String line = br.readLine();
-
             while(line != null){
                 System.out.println(line);
                 line = br.readLine();
             }
         }catch (IOException e ){
             System.out.println("Error : + " + e.getMessage());
-        }
-        finally {
-            try {
-                if (br != null) {
-                    br.close();
-                }
-                if (fr != null) {
-                    br.close();
-                }
-            }
-            catch (IOException e ){
-                e.printStackTrace();
-        }
         }
     }
 }
