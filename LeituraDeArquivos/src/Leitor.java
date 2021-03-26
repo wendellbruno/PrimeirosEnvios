f63398a9a68384af3fb60d2800d30dbbcf03eff7
wendellbruno <wendell_101@hotmail.com>
2021-03-26 16:36:14 -0300

    Versão 1.0

--- LeituraDeArquivos/src/Leitor.java
+++ LeituraDeArquivos/src/Leitor.java
@@ -1,26 +1,41 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
+
 
 public class Leitor {
     public static void main(String[] args) {
 
-        File file = new File("C:\\temp\\in.txt");
-        Scanner scan = null;
+        String path = "c:\\temp\\in.txt";
+        FileReader fr = null;
+        BufferedReader br = null;
+
         try{
-            scan = new Scanner(file);
-             while (scan.hasNextLine()){
-                System.out.println(scan.nextLine());
-             }
-        }
-        catch (IOException e){
-            System.out.println("Error : " + e.getMessage());
+            fr = new FileReader(path);
+            br = new BufferedReader(fr);
+
+            String line = br.readLine();
+
+            while(line != null){
+                System.out.println(line);
+                line = br.readLine();
+            }
+        }catch (IOException e ){
+            System.out.println("Error : + " + e.getMessage());
         }
         finally {
-            if(scan != null){
-                scan.close();
+            try {
+                if (br != null) {
+                    br.close();
+                }
+                if (fr != null) {
+                    br.close();
+                }
             }
+            catch (IOException e ){
+                e.printStackTrace();
+        }
         }
     }
 }
