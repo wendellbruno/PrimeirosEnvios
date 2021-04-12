16be14d0dd7d1195790319340a3729c3988150c8
wendellbruno <wendell_101@hotmail.com>
2021-04-12 11:05:54 -0300

    Versao sem lambda declarada

--- IntroducaoLambda/src/application/Program.java
+++ IntroducaoLambda/src/application/Program.java
@@ -18,9 +18,7 @@ public class Program {
 
         double factor = 1.1;
 
-        Consumer<Product> cons = p -> {
-            p.setPrice(p.getPrice() * factor);
-        };
+        list.forEach(p -> p.setPrice(p.getPrice()*factor));
         list.forEach(System.out::println);
     }
 
