5b0d4d15ad2502fd4f135278fd01ff400863eab1
wendellbruno <wendell_101@hotmail.com>
2021-04-12 10:58:44 -0300

    Versao sem metodo Statico

--- IntroducaoLambda/src/entites/Product.java
+++ IntroducaoLambda/src/entites/Product.java
@@ -31,6 +31,9 @@ public class Product {
     public static void staticPriceUpdate(Product p){
         p.setPrice(p.getPrice()*1.1);
     }
+    public void nonStaticPriceUpdate(){
+        setPrice(getPrice()*1.1);
+    }
 
     @Override
     public String toString() {
