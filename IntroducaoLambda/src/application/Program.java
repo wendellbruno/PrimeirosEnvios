890380e54cb5d6ef7614b568b3d604d5a4d0fa93
wendellbruno <wendell_101@hotmail.com>
2021-04-12 11:03:17 -0300

    Usando versao com Lambda Declarada

--- IntroducaoLambda/src/application/Program.java
+++ IntroducaoLambda/src/application/Program.java
@@ -5,6 +5,7 @@ import util.PriceUpdate;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Program {
     public static void main(String[] args) {
@@ -15,8 +16,11 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
+        double factor = 1.1;
 
-        list.forEach(Product::nonStaticPriceUpdate);
+        Consumer<Product> cons = p -> {
+            p.setPrice(p.getPrice() * factor);
+        };
         list.forEach(System.out::println);
     }
 
