aaf4082c82feb2dd8bbbe9da65eab0f5d27bb0d3
wendellbruno <wendell_101@hotmail.com>
2021-04-12 10:52:46 -0300

    Iniciando Consumer com Interface

--- IntroducaoLambda/src/application/Program.java
+++ IntroducaoLambda/src/application/Program.java
@@ -1,28 +1,23 @@
 package application;
 
 import entites.Product;
-import util.ProductPredicate;
+import util.PriceUpdate;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.function.Predicate;
 
 public class Program {
     public static void main(String[] args) {
         List<Product> list = new ArrayList<>();
 
-        list.add(new Product("TV", 900.00));
-        list.add(new Product("Notebook", 1200.00));
-        list.add(new Product("Tablet", 450.00));
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
 
-       
 
-        list.removeIf(p -> p.getPrice() >= 1000);
-
-        for (Product p : list) {
-            System.out.println(p);
-        }
+        list.forEach(new PriceUpdate());
+        list.forEach(System.out::println);
     }
 
 }
