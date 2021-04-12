bb654ae4555227e3986ff0be5f35e61a5f418b3e
wendellbruno <wendell_101@hotmail.com>
2021-04-12 10:31:12 -0300

    Iniciando Interfaces Lambda

--- IntroducaoLambda/src/application/Program.java
+++ IntroducaoLambda/src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
 import entites.Product;
+import util.ProductPredicate;
 
+import javax.sql.rowset.Predicate;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -10,13 +12,12 @@ public class Program {
     public static void main(String[] args) {
         List<Product> list = new ArrayList<>();
 
-        list.add(new Product("TV",900.00));
-        list.add(new Product("Notebook",1200.00));
-        list.add(new Product("Tablet",450.00));
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Notebook", 1200.00));
+        list.add(new Product("Tablet", 450.00));
 
+        list.removeIf(new ProductPredicate());
 
-
-        list.sort((p1,p2) -> p1.getPrice().compareTo(p2.getPrice()));
         for (Product p : list) {
             System.out.println(p);
         }
